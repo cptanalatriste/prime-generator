ac6f24ab945d10018153b9030e806aaf64dadb02
Carlos Gavidia-Calderon <cgavidiac@gmail.com>
2022-05-24 09:37:05 +0100

    Initial skeleton code.

---
+++ src/main/java/uk/ac/open/primegenerator/Main.java
@@ -0,0 +1,14 @@
+package uk.ac.open.primegenerator;
+
+public class Main {
+
+    public int generatePrime() {
+        return 7;
+    }
+
+    public static void main(String... args) {
+        Main primeGenerator = new Main();
+        System.out.println(primeGenerator.generatePrime());
+    }
+
+}
---
+++ src/test/java/uk/ac/open/primegenerator/MainTest.java
@@ -0,0 +1,16 @@
+package uk.ac.open.primegenerator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.apache.commons.math3.primes.Primes;
+
+public class MainTest {
+
+    private final Main primeGenerator = new Main();
+
+    @Test
+    public void testSinglePrimeGeneration() {
+        int generatedPrime = primeGenerator.generatePrime();
+        Assert.assertTrue(Primes.isPrime(generatedPrime));
+    }
+}
